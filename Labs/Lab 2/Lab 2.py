f624377fc85cf5afbfad75626a9bb66e60a883c8
veiyas <david.rk97@gmail.com>
2021-04-15 12:00:23 +0200

    Initial clustering plot

--- Labs/Lab 2/Lab 2.py
+++ Labs/Lab 2/Lab 2.py
@@ -1,17 +1,30 @@
 import pandas as pd
-from sklearn.cluster import OPTICS
+from sklearn.cluster import OPTICS, DBSCAN
+import matplotlib.pyplot as plt
 import numpy as np
 
-colnames = ['a', 'b', 'GazeEventDuration', 'c', 'GazePointX', 'GazePointY' ]
-df = pd.read_csv('Lab 2 Data.tsv', sep='\t', names=colnames)
+# colnames = ['a', 'b', 'GazeEventDuration', 'c', 'GazePointX', 'GazePointY' ]
+df = pd.read_csv('Lab 2 Data.tsv', sep='\t')
 #del df['Unnamed: 6']
-print(df)
+# print(df)
 
-my_data = np.genfromtxt('my_file.csv', delimiter='\t')
+my_data = np.genfromtxt('Lab 2 Data.tsv', delimiter='\t')
 
-test = [df.GazeEventDuration.tolist(), df.GazePointX.tolist(), df.GazePointY.tolist()]
-print(test)
+test = df.filter(items=['GazePointX(px)', 'GazePointY(px)', 'GazeEventDuration(mS)']).values
+
+data = np.array([df['GazeEventDuration(mS)'][1:], df['GazePointX(px)'][1:], df['GazePointY(px)'][1:]])
 
 clustering = OPTICS(min_samples=2).fit(test)
-print("a")
+# cluster = DBSCAN(eps=3, min_samples=10).fit(test)
+print(clustering.labels_)
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = np.array(df['GazeEventDuration(mS)'])
+y = np.array(df['GazePointX(px)'])
+z = np.array(df['GazePointY(px)'])
+
+ax.scatter(x,y,z, marker="s", c=clustering.labels_, s=40, cmap="RdBu")
+plt.xlabel('TIME')
+
+plt.show()
