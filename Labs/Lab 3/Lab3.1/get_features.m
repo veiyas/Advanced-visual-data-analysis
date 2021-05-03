23ab79506bdd79e4cf66a53389497d34bad431e7
veiyas <david.rk97@gmail.com>
2021-05-03 16:47:36 +0200

    Hopefully finished lab 3

--- Labs/Lab 3/Lab3.1/get_features.m
+++ Labs/Lab 3/Lab3.1/get_features.m
@@ -7,19 +7,22 @@ imHSV = rgb2hsv(im);
 [S_hist, ~] = histcounts(reshape(imHSV(:,:,2), 1, size(imHSV,1)*size(imHSV,2)), [0:1/100:1]);
 [V_hist, ~] = histcounts(reshape(imHSV(:,:,3), 1, size(imHSV,1)*size(imHSV,2)), [0:1/100:1]);
 
+H_hist = H_hist ./ (size(imHSV,1)*size(imHSV,2));
+S_hist = S_hist ./ (size(imHSV,1)*size(imHSV,2));
+V_hist = V_hist ./ (size(imHSV,1)*size(imHSV,2));
+
 H_mean = mean(mean(imHSV(:,:,1)));
 S_mean = mean(mean(imHSV(:,:,2)));
 V_mean = mean(mean(imHSV(:,:,3)));
 
-H_std = std(reshape(imHSV(:,:,1), 1, size(imHSV,1)*size(imHSV,2)))
-S_std = std(reshape(imHSV(:,:,2), 1, size(imHSV,1)*size(imHSV,2)))
-V_std = std(reshape(imHSV(:,:,3), 1, size(imHSV,1)*size(imHSV,2)))
+H_std = std(reshape(imHSV(:,:,1), 1, size(imHSV,1)*size(imHSV,2)));
+S_std = std(reshape(imHSV(:,:,2), 1, size(imHSV,1)*size(imHSV,2)));
+V_std = std(reshape(imHSV(:,:,3), 1, size(imHSV,1)*size(imHSV,2)));
 
-BW = edge(rgb2gray(im), 'canny');
-[houghRes,T,R] = hough(BW,'RhoResolution', 0.5, 'Theta', -90:0.5:89);
+lbp = extractLBPFeatures(rgb2gray(im));
 
 features_struct = struct('H',H_hist,'S', S_hist, 'V', V_hist,...
     'H_mean', H_mean, 'S_mean', S_mean, 'V_mean', V_mean,...
-    'H_std', H_std, 'S_std', S_std, 'V_std', V_std);
+    'H_std', H_std, 'S_std', S_std, 'V_std', V_std, 'LBP', lbp);
 end
 
--- Labs/Lab 3/Lab3.1/main.m
+++ Labs/Lab 3/Lab3.1/main.m
@@ -6,21 +6,44 @@ imHSV = rgb2hsv(im);
 [S_hist, ~] = histcounts(reshape(imHSV(:,:,2), 1, size(imHSV,1)*size(imHSV,2)), [0:1/100:1]);
 [V_hist, ~] = histcounts(reshape(imHSV(:,:,3), 1, size(imHSV,1)*size(imHSV,2)), [0:1/100:1]);
 
-S1 = get_features(im)
-S2 = get_features(imread('10.jpg'))
-
 BW = edge(rgb2gray(im), 'canny');
 [houghRes,T,R] = hough(BW, 'Theta', -90:0.5:89);
 
+lbpTest = extractLBPFeatures(rgb2gray(im));
+
 
 %%
-base = get_features(imread('11.jpg'));
+chosenBase = 11;
+if chosenBase < 10
+    chosenBasePrefix = strcat('0',int2str(chosenBase));
+else
+    chosenBasePrefix = int2str(chosenBase);
+end
+allPathsPrefix = ['010203040506070809101112'];
+paths = erase(allPathsPrefix, chosenBasePrefix);
 
-paths = ['0102030405060708091012'];
+S = get_features(imread(strcat(chosenBasePrefix, '.jpg')));
+baseFVec = get_feature_vector(S);
+
 pathSuffix = '.jpg';
 fullPath = '';
 
+lowestDiffs = [];
 for i = 1:2:size(paths,2)
     fullPath = strcat(paths(i:i+1), pathSuffix);
     S = get_features(imread(fullPath));
+    
+    fVec = get_feature_vector(S);
+    lowestDiffs((i+1)/2,:) = [norm(baseFVec - fVec)/1000];
 end
+
+[B, I] = sort(lowestDiffs);
+sortedSimilarity = I + double(I >= chosenBase)
+
+
+%%
+function [feature_vector] = get_feature_vector(S)
+    feature_vector = [S.H S.S S.V S.H_mean S.S_mean S.V_mean S.H_std S.S_std S.V_std S.LBP];
+end
+
+
