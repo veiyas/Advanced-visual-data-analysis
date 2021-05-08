e71be5121afd7f3cc4b23ac5c09693f6fe0335f1
Algot Sandahl <algot.sandahl@gmail.com>
2021-05-08 12:50:25 +0200

    Make lab3.2 split by sentence instead of by line, and various other improvements

--- Labs/Lab 3/Lab3.2/lab3_2.py
+++ Labs/Lab 3/Lab3.2/lab3_2.py
@@ -1,5 +1,34 @@
 import string
 import copy
+import nltk.data
+from nltk.tokenize import PunktSentenceTokenizer
+
+
+def remove_punctuation(sentence_list):
+  return [sentence.translate(str.maketrans('','', string.punctuation)) 
+          for sentence in sentence_list]
+
+def make_lower(sentence_list):
+  return [sentence.lower() for sentence in sentence_list]
+
+def replace_words_with_numbers(texts):
+  texts_numbers = copy.deepcopy(texts);
+  dictionary = {}
+  dict_counter = 0
+  for text in texts_numbers:
+    for i in range(len(text)):
+      sentence = text[i]
+      word_indices = []
+      for curr_word in sentence.split():
+        if curr_word in dictionary:
+          word_indices.append(str(dictionary[curr_word]))
+        else:
+          dictionary[curr_word] = dict_counter
+          word_indices.append(str(dict_counter))
+          dict_counter += 1
+      text[i] = ' '.join(word_indices)
+  return dictionary
+  
 
 texts = []
 for i in range(1,11):
@@ -7,88 +36,43 @@ for i in range(1,11):
   texts.append(f.read())
   f.close()
 
-# print(len(texts))
+texts = [text.replace('\n', ' ') for text in texts]
 
-texts = [text.lower() for text in texts]
-texts = [text.translate(str.maketrans('','', string.punctuation)) for text in texts]
-texts = [text.splitlines() for text in texts]
-texts = [[sentence for sentence in text if sentence] for text in texts]
-# TODO KOM PÅ ATT JAG NU DELAR PÅ RADER ISTÄLLET FÖR MENINGAR
+# Divide text into sentences
+nltk.download('punkt')
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+texts = [tokenizer.tokenize(text) for text in texts]
 
-# # Store the words as elements in an array for each of the texts
-# texts_words = [text.split() for text in texts]
+texts = [remove_punctuation(sentences) for sentences in texts]
+texts = [make_lower(sentences) for sentences in texts]
 
 # Replacing the words with numbers and storing the words in a dictionary
-texts_numbers = copy.deepcopy(texts);
-dictionary = {}
-dict_counter = 0
-for text in texts_numbers:
-  for i in range(len(text)):
-    sentence = text[i]
-    word_indices = []
-    for curr_word in sentence.split():
-      if curr_word in dictionary:
-        word_indices.append(str(dictionary[curr_word]))
-      else:
-        dictionary[curr_word] = dict_counter
-        word_indices.append(str(dict_counter))
-        dict_counter += 1
-    text[i] = ' '.join(word_indices)
-
+texts_numbers = copy.deepcopy(texts)
+dictionary = replace_words_with_numbers(texts_numbers)
 
 # Compare the sequences
-for idx1, text1 in enumerate(texts_numbers):
-  for idx2, text2 in enumerate(texts_numbers):
-    # print(f'\t{idx1} {idx2}')
+print('\n\nMatches found')
+print('=============')
+for idx1 in range(len(texts_numbers)-1):
+  text1 = texts_numbers[idx1]
+  for idx2 in range(idx1+1, len(texts_numbers)):
+    text2 = texts_numbers[idx2]
+  
     if text1 == text2:
       continue
-    i = 0
-    for sentence1 in text1:
-      i += 1
-      if sentence1 in text2:
-        # print(i)
-        print(texts[idx1][i])
-    #   for sentence2 in text2:
 
-        
+    for i, sentence1 in enumerate(text1):
+      # Skip short sentences---these are mostly just noise,
+      # either so unoriginal that they are of no interest,
+      # or incorrectly split by the sentence tokenization
+      if len(sentence1.split()) <= 4:
+        continue
 
+      try:
+        text2_find_sentence_idx = text2.index(sentence1)
+        print(f'[between text {idx1+1} and {idx2+1}] {texts[idx1][i]}')
+        # print(f'[Match between text {idx1+1} and {idx2+1}] {texts[idx1][i]}  ===  {texts[idx2][text2_find_sentence_idx]}')
+        # print(f'\t= {texts[idx2][text2_find_sentence_idx]}')
+      except:
+        pass
 
-
-
-
-
-#__________________________
-# texts = [text.lower() for text in texts]
-# texts = [text.translate(str.maketrans('','', string.punctuation)) for text in texts]
-# texts = [text.replace('\n', ' ') for text in texts]
-
-# # Store the words as elements in an array for each of the texts
-# texts_words = [text.split() for text in texts]
-
-# # Replacing the words with numbers and storing the words in a dictionary
-# dictionary = {}
-# dict_counter = 0;
-# for words in texts_words:
-#   for i in range(len(words)):
-#     curr_word = words[i]
-#     if curr_word in dictionary:
-#       words[i] = dictionary[curr_word]
-#     else:
-#       dictionary[curr_word] = dict_counter
-#       words[i] = dict_counter
-#       dict_counter += 1
-
-# # Compare the sequences
-# # I'm really not sure how to do this efficiently
-
-# # for text1 in texts_words:
-# #   for text2 in texts_words:
-# #     for text1_word in text1:
-# #       for text2_word in text2:
-        
-
-# # Eg kanske man skulle dela upp i meningar?
-
-
-# print(texts_words[0])
-# print(len(texts_words[0]))
